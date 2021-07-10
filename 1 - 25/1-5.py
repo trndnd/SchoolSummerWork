8360dc27d0faecfcfe560819ef7a118d67c4cb81
aaronmojica <originalcoffee20184@gmail.com>
2021-07-10 19:16:14 +0100

    Second commit

--- 1 - 25/1-5.py
+++ 1 - 25/1-5.py
@@ -1,5 +1,5 @@
 #1
-print("This is my first program")
-print("It shows messages")
-print("Sometimes on fifferent lines")
+#print("This is my first program")
+#print("It shows messages")
+#print("Sometimes on fifferent lines")
 #2
