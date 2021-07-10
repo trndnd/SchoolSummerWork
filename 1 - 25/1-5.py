ab4a034089c26a98da77fff16b15a21024cb481f
aaronmojica <originalcoffee20184@gmail.com>
2021-07-10 19:28:25 +0100

    Finished fourth and fifth task

--- 1 - 25/1-5.py
+++ 1 - 25/1-5.py
@@ -3,9 +3,20 @@
 #print("It shows messages")
 #print("Sometimes on different lines")
 #2
-food = "Rice"
-print(food)
+#food = "Rice"
+#print(food)
 #3
-print("What's your favourite film? ")
-Favourite_Film = input()
-print(f"I also like watching {Favourite_Film}")
+#print("What's your favourite film? ")
+#Favourite_Film = input()
+#print(f"I also like watching {Favourite_Film}")
+#4
+#print("What's your firstname,surname,favourite subject and age? (Have a space between them)")
+#First_Name,Surname,Favourite_Subject,age = input().split()
+#print(First_Name)
+#print(Surname)
+#print(Favourite_Subject)
+#print(age)
+#5
+print("What's your name? ")
+name = input()
+print(5 * f" {name}")
