023b45f01b7c13db1890e671a224e6dcdf6ffc1e
aaronmojica <originalcoffee20184@gmail.com>
2021-07-10 19:21:19 +0100

    Completed second and third task

--- 1 - 25/1-5.py
+++ 1 - 25/1-5.py
@@ -1,5 +1,11 @@
 #1
 #print("This is my first program")
 #print("It shows messages")
-#print("Sometimes on fifferent lines")
+#print("Sometimes on different lines")
 #2
+food = "Rice"
+print(food)
+#3
+print("What's your favourite film? ")
+Favourite_Film = input()
+print(f"I also like watching {Favourite_Film}")
