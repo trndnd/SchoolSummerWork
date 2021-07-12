3f85daaf2a6a521e3d014bd66ca91711875c3c84
aaronmojica <originalcoffee20184@gmail.com>
2021-07-12 20:06:07 +0100

    Finished task 14,15 and 16

--- Arithmetic operations/12 - 17/12 - 17.py
+++ Arithmetic operations/12 - 17/12 - 17.py
@@ -1,7 +1,28 @@
-#12
+#13
 #x = 5
 #y = 3
 #z = 10
 #answer = x * y * z
 #print(answer)
-#13
+#14
+#Alexs_Money = 20
+#Cost_Of_Pens = 5
+#Cost_Of_Pencils = 3
+#Alexs_Money -= (Cost_Of_Pencils + Cost_Of_Pens)
+#print(Alexs_Money)
+#15
+#print("How many apples do you need?")
+#Apples_Needed = int(input())
+#print("How many people are you going to share these apples with? ") 
+#People = int(input())
+#Remainder = Apples_Needed % People
+#print(Remainder)
+#16
+#ome_Team = input("Whats the name of the home team? ")#Not really needed
+#Home_Team_Goals = int(input("How many goals did the home team score? "))
+#Away_Team = input("Whats the name of the away team? ")#Not really needed
+#Away_Team_Goals = int(input("How many goals did the away team score? "))
+#Difference = Home_Team_Goals - Away_Team_Goals
+#print(Difference)
+#17 not there
+
