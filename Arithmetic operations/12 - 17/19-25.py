5e5408d17c245ce7fada76ca5e9b19c40ab9dda6
aaronmojica <originalcoffee20184@gmail.com>
2021-07-12 21:03:21 +0100

    Finished task 19 and 20

---
+++ Arithmetic operations/12 - 17/19-25.py
@@ -0,0 +1,11 @@
+#19
+#Mancity_Goals = 60
+#ManUnited_Goals = 51
+#print(Mancity_Goals > ManUnited_Goals)
+#20
+#print("How old are you? ")
+#Age = int(input())
+#if Age >= 18:
+    #print("You are old enough to vote")
+#else:
+    #print("You are not allowed to vote")
