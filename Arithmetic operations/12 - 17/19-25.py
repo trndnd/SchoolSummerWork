aa036c30fb734aa41d166e9c22ed999c296db17f
aaronmojica <originalcoffee20184@gmail.com>
2021-07-13 07:42:29 +0100

    Removed hashtags

--- Arithmetic operations/12 - 17/19-25.py
+++ Arithmetic operations/12 - 17/19-25.py
@@ -3,48 +3,48 @@
 #ManUnited_Goals = 51
 #print(Mancity_Goals > ManUnited_Goals)
 #20
-#print("How old are you? ")
-#Age = int(input())
-#if Age >= 18:
-    #print("You are old enough to vote")
-#else:
-    #print("You are not allowed to vote")
+print("How old are you? ")
+Age = int(input())
+if Age >= 18:
+    print("You are old enough to vote")
+else:
+    print("You are not allowed to vote")
 #21
-#print("What is your name? ")
-#Name = input()
-#if Name == "Tom":
-#    print("Welcome Tom")
-#else:
-#    print("Hello stranger")
+print("What is your name? ")
+Name = input()
+if Name == "Tom":
+    print("Welcome Tom")
+else:
+    print("Hello stranger")
 #22
-#print("Hello user")
-#print("How are you feeling? (Happy or sad)")
-#Emotion = input()
-#if Emotion.lower() == "happy":
-#    print("Glad to hear it!")
-#elif Emotion.lower() == "sad":
-#    print("I don't have any jokes")
-#else:
-#    print("Error")
+print("Hello user")
+print("How are you feeling? (Happy or sad)")
+Emotion = input()
+if Emotion.lower() == "happy":
+    print("Glad to hear it!")
+elif Emotion.lower() == "sad":
+    print("I don't have any jokes")
+else:
+    print("Error")
 #23
-#print("Waht mark did you get on your test? ")
-#Mark = int(input())
-#if Mark >= 90:
-    #print("A*")
-#elif Mark >= 80:
-#    print("A")
-#elif Mark >= 70:
-#    print("B")
-#elif Mark >= 60:
-#    print("C")
-#else:
-#    print("Fail")
+print("Waht mark did you get on your test? ")
+Mark = int(input())
+if Mark >= 90:
+    print("A*")
+elif Mark >= 80:
+    print("A")
+elif Mark >= 70:
+    print("B")
+elif Mark >= 60:
+    print("C")
+else:
+    print("Fail")
 #24
-#print("What's your favourite band? ")
-#Favourite_Band = input()
-#print("What's your favourite song? ")
-#Favourite_Song = input()
-#print(f"Your favourite band is {Favourite_Band} and your favourite song is {Favourite_Song}")
+print("What's your favourite band? ")
+Favourite_Band = input()
+print("What's your favourite song? ")
+Favourite_Song = input()
+print(f"Your favourite band is {Favourite_Band} and your favourite song is {Favourite_Song}")
 #25
 Number1 = int(input("What is your first number? "))
 Number2 = int(input("What is your second number? "))
