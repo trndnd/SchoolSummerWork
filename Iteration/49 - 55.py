f9cba8cb3ba8209e325082b677e3466d3635f8e6
aaronmojica <originalcoffee20184@gmail.com>
2021-07-14 20:31:24 +0100

    Finished tasks 49 - 55

---
+++ Iteration/49 - 55.py
@@ -0,0 +1,28 @@
+#49
+'''
+for x in range(1,100):
+    print(x)
+#50
+for x in range(100,0,-1):
+    print(x)
+#51
+Number = int(input("Enter a number "))
+for x in range(1,11):
+    print(x * Number)
+#52
+Number = int(input("Enter a number "))
+for x in range(Number):
+    print("Word")
+#53
+Name = input("What is your name? ")
+How_Many_Repeats_Wanted = int(input("How many times do you want the name to repeat? "))
+for x in range(How_Many_Repeats_Wanted):
+    print(Name)
+#54
+Number = int(input("Enter a number "))
+for x in range(1,6):
+    print(f"{x} x {Number} = {x * Number}")'''
+#55
+word = input("Enter a word ")
+for x in range(len(word)):
+    print(word)
