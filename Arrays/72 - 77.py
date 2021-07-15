140e81ea5d20a6c08fc1c113ddf130cefd31011b
aaronmojica <originalcoffee20184@gmail.com>
2021-07-15 18:25:51 +0100

    Completed tasks 72 - 75

---
+++ Arrays/72 - 77.py
@@ -0,0 +1,21 @@
+import numpy as np
+#72
+'''
+Movie_Array = ["Weathering with you","your name","End game","Black panther"]
+print(Movie_Array[0])
+print(Movie_Array[-1])
+#73
+Numbers_Array = [23,24,512,53,542,532]
+print(Numbers_Array[3])
+#74 
+array = [4,2,3,5,1,10.12]
+Number_We_Are_Looking_For = int(input("What number are you looking for? "))
+for number in array:
+    if number == Number_We_Are_Looking_For:
+        print(f"{Number_We_Are_Looking_For } found!")
+#75
+Games_Array = []
+for x in range(4):
+    Game = input("Enter a game ")
+    Games_Array.append(Game)
+print(Games_Array)'''
