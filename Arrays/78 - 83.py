f5ed299c7db1b8273247127aaec9f8d70db6d676
aaronmojica <originalcoffee20184@gmail.com>
2021-07-16 19:55:20 +0100

    Finished task 83

--- Arrays/78 - 83.py
+++ Arrays/78 - 83.py
@@ -1,5 +1,4 @@
 #78 and 79
-'''
 Games_Array = []
 for x in range(3):
     Game = input("Name a game ")
@@ -49,14 +48,47 @@ while Finished == False:
     else:
         print("Not a valid gender")
 print(f"{Male_Counter} males")
-print(f"{Female_Counter} females")'''
+print(f"{Female_Counter} females")
 #83
 class Game_Menu:
-    def __init__(self,Games_Array):
-        self.Games_Array = Games_Array
+    def __init__(self):
+        self.Games_Array = []
 
-x = 0
-while x < 1:
-    print("Do you want to add,edit,delete a game or print all of your games? ")
-    Decision = input()
-    
+    def Adding(self):
+        Game = input("What game do you want to add? ")
+        self.Games_Array.append(Game)
+
+    def Editing(self):
+        print(self.Games_Array)
+        Value_Being_Edited = input("Which one do you want to change? ")
+        What_It_Will_Be_Changed_To = input("What would you like it to change to? ")
+        Value_Being_Edited_Position = self.Games_Array.index(Value_Being_Edited)
+        self.Games_Array[Value_Being_Edited_Position] = What_It_Will_Be_Changed_To
+
+    def printing(self):
+        print(self.Games_Array)
+
+    def deleting(self):
+        print(self.Games_Array)
+        Game_Wanting_To_Be_Deleted = input("What game do you want to delete? ")
+        self.Games_Array.remove(Game_Wanting_To_Be_Deleted)
+
+    def Menu_Screen(self):
+        x = 0
+        while x < 1:
+            print("Do you want to add,edit,delete a game or print all of your games? (or quit)")
+            Decision = input()
+            if Decision.lower() == "add":
+                Game_Menu.Adding(self)
+            elif Decision.lower() == "edit":
+                Game_Menu.Editing(self)
+            elif Decision.lower() == "print":
+                Game_Menu.printing(self)
+            elif Decision.lower() == "delete":
+                Game_Menu.deleting(self)
+            elif Decision.lower() == "quit":
+                x = 1
+            else:
+                print("Not a valid option")
+y = Game_Menu()
+y.Menu_Screen()
