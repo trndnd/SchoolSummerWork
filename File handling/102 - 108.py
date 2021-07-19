3a5a274e59aa27670e1e07e4e88483ecac916c0d
aaronmojica <originalcoffee20184@gmail.com>
2021-07-19 19:20:44 +0100

    Finished task 106

--- File handling/102 - 108.py
+++ File handling/102 - 108.py
@@ -19,10 +19,28 @@ print("How many numbers do you want to enter? ")
 How_Many_Numbers_Wanted_To_Add_To_File = int(input())
 for x in range(How_Many_Numbers_Wanted_To_Add_To_File):
     Number = int(input("Enter a number "))
-    Numbers_File.write(f"{Number} \n")'''
+    Numbers_File.write(f"{Number} \n")
 #105
 Details_File = open("Task105.txt","a")
 First_Name = input("What is your first name? ")
 Surname = input("What is your surname? ")
 Age = int(input("How old are you? "))
-Address = input("What's your address? ")
+Address = input("What's your address? ")'''
+#106
+MyFile_Appending = open("Task106.txt","w")
+MyFile_Reading = open("Task106.txt","r")
+for x in range(5):
+    Name_Of_Game = input("Enter a game ")
+    MyFile_Appending.write(f"{Name_Of_Game} \n")
+MyFile_Appending.close()
+MyString = ""
+MyList_Of_Games = []
+x = 0
+while x < 1:
+    LineValue = MyFile_Reading.readline()
+    if LineValue == "":
+        x = 1
+    else:
+        MyString += f"{LineValue.strip()},"
+MyList_Of_Games.append(MyString)
+print(MyList_Of_Games)
