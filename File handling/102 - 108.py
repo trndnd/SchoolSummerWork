d7d2e279a0d5b0ec9a841f9159f7f93156c4e47d
aaronmojica <originalcoffee20184@gmail.com>
2021-07-19 19:54:17 +0100

    Finished tasks 107 and 108

--- File handling/102 - 108.py
+++ File handling/102 - 108.py
@@ -1,5 +1,5 @@
+import random
 #102
-'''
 Numbers_File = open("Numbers.txt","a")
 for x in range(5):
     print("What is your number? ")
@@ -25,7 +25,7 @@ Details_File = open("Task105.txt","a")
 First_Name = input("What is your first name? ")
 Surname = input("What is your surname? ")
 Age = int(input("How old are you? "))
-Address = input("What's your address? ")'''
+Address = input("What's your address? ")
 #106
 MyFile_Appending = open("Task106.txt","w")
 MyFile_Reading = open("Task106.txt","r")
@@ -44,3 +44,21 @@ while x < 1:
         MyString += f"{LineValue.strip()},"
 MyList_Of_Games.append(MyString)
 print(MyList_Of_Games)
+#107
+Reading_File_Football_Names = open("Football_Name.txt","r")
+Random_Number = random.randint(0,7)
+print(Random_Number)
+Content = Reading_File_Football_Names.readlines()
+LineValue = Content[Random_Number].split(',')
+print(f"The team is {LineValue[0]}")
+Player_Name = LineValue[1].split(" ")
+print(f"The first letter of in the first name is {Player_Name[0][0]}")
+print(f"The first letter of the surname is {Player_Name[1][0]}")
+#108
+Name = input("What is your name? ")
+Homework_File = open(f"{Name}.txt","w")
+Homework_Tasks_They_Need_To_Complete = int(input())
+for x in range(Homework_Tasks_They_Need_To_Complete):
+    Homework_Name = input("What is the name of the homework task? ")
+    Homework_File.write(f"Task {x + 1}: {Homework_Name} \n")
+Homework_File.close()
